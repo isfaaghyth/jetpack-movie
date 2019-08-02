9098cf8c79545245296e01a3db11067b36e35fea
isfaaghyth <isfaaghyth@gmail.com>
2019-08-02 14:32:16 +0700

    run movies feature onStart() on app module

--- app/src/main/java/isfaaghyth/app/jetmovie/MainActivity.kt
+++ app/src/main/java/isfaaghyth/app/jetmovie/MainActivity.kt
@@ -2,6 +2,7 @@ package isfaaghyth.app.jetmovie
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import isfaaghyth.app.movies.ui.MovieActivity
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,4 +10,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    override fun onStart() {
+        super.onStart()
+        startActivity(MovieActivity.intent(this))
+    }
+
 }
