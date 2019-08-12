46fac36f31c3db229feed39e474c6fc2c762c151
isfaaghyth <isfaaghyth@gmail.com>
2019-08-12 23:57:23 +0700

    change extension name for fetch state

--- libraries/abstraction/src/main/java/isfaaghyth/app/abstraction/util/CoroutinesUtil.kt
+++ libraries/abstraction/src/main/java/isfaaghyth/app/abstraction/util/CoroutinesUtil.kt
@@ -2,10 +2,12 @@ package isfaaghyth.app.abstraction.util
 
 import java.io.IOException
 
-suspend fun <T: Any> launchCall(call: suspend () -> ResultState<T>): ResultState<T> {
+suspend fun <T: Any> fetchState(call: suspend () -> ResultState<T>): ResultState<T> {
     return try {
         call.invoke()
     } catch (e: Exception) {
+        ResultState.Error(IOException(e.message))
+    } catch (e: Throwable) {
         ResultState.Error(IOException(e.message))
     }
 }
