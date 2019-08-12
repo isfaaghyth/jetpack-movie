13e97e7ada3476d8959b6715a1880434ad71a7a0
isfaaghyth <isfaaghyth@gmail.com>
2019-08-13 01:27:05 +0700

    replace Deferred<> with suspend function 🤩

--- data/src/main/java/isfaaghyth/app/data/service/NetworkServices.kt
+++ data/src/main/java/isfaaghyth/app/data/service/NetworkServices.kt
@@ -1,22 +1,36 @@
 package isfaaghyth.app.data.service
 
+import isfaaghyth.app.data.entity.Movie
 import isfaaghyth.app.data.entity.Movies
 import isfaaghyth.app.data.entity.TVShows
-import kotlinx.coroutines.Deferred
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface NetworkServices {
 
     @GET("movie/popular")
-    fun getPopularMovie(
+    suspend fun getPopularMovie(
         @Query("api_key") apiKey: String
-    ): Deferred<Response<Movies>>
+    ): Response<Movies>
 
     @GET("tv/popular")
-    fun getPopularTVShow(
+    suspend fun getPopularTVShow(
         @Query("api_key") apiKey: String
-    ): Deferred<Response<TVShows>>
+    ): Response<TVShows>
+
+    @GET("movie/{movie_id}")
+    suspend fun getMovieDetail(
+        @Path("movie_id") movieId: String,
+        @Query("api_key") apiKey: String
+    ): Response<Movie>
+
+    @GET("{type}/{movie_id}")
+    fun <T> getDetail(
+        @Path("type") type: String,
+        @Path("movie_id") movieId: String,
+        @Query("api_key") apiKey: String
+    ): Response<T>
 
 }
