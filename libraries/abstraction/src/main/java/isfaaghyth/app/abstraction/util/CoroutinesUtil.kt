20bcc9cf9af69c89c31e220fc723f21b8da673d2
isfaaghyth <isfaaghyth@gmail.com>
2019-08-13 01:25:16 +0700

    change state error from exception into message string

--- libraries/abstraction/src/main/java/isfaaghyth/app/abstraction/util/CoroutinesUtil.kt
+++ libraries/abstraction/src/main/java/isfaaghyth/app/abstraction/util/CoroutinesUtil.kt
@@ -1,13 +1,15 @@
 package isfaaghyth.app.abstraction.util
 
-import java.io.IOException
+import java.net.ConnectException
 
 suspend fun <T: Any> fetchState(call: suspend () -> ResultState<T>): ResultState<T> {
     return try {
         call.invoke()
+    } catch (e: ConnectException) {
+        ResultState.Error(e.message)
     } catch (e: Exception) {
-        ResultState.Error(IOException(e.message))
+        ResultState.Error(e.message)
     } catch (e: Throwable) {
-        ResultState.Error(IOException(e.message))
+        ResultState.Error(e.message)
     }
 }
